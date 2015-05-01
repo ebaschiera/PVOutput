20c3796d58b63fefa9d7b31b143915c73dd73f96
Ermanno Baschiera <ebaschiera@gmail.com>
2015-05-01 17:47:31 +0200

    added getoutput method

--- src/PVOutput.php
+++ src/PVOutput.php
@@ -14,6 +14,7 @@ class PVOutput {
 
   const PVOUTPUT_ADDSTATUS_URL = 'http://pvoutput.org/service/r2/addstatus.jsp';
   const PVOUTPUT_ADDOUTPUT_URL = 'http://pvoutput.org/service/r2/addoutput.jsp';
+  const PVOUTPUT_GETOUTPUT_URL = 'http://pvoutput.org/service/r2/getoutput.jsp';
 
   /**
    * Class constructor with PVOutput parameters init
@@ -30,19 +31,18 @@ class PVOutput {
   }
 
   /**
-   * Adds a Status to PVOutput.org via the Add Status Service.
-   * This method will send instant values (v2 and/or v4 fields according to API specs).
+   * Adds a Status to PVOutput.org via the Add Status Service
    * 
-   * @param int $generation_amount watts (v2 parameter in API)
-   * @param int $consumption_amount watts (v4 parameter in API)
-   * @param \DateTime $timestamp (d and t parameters in API)
+   * @param int $production_amount
+   * @param int $consumption_amount
+   * @param \DateTime $timestamp
    * @return boolean
    * @link http://www.pvoutput.org/help.html#api-addstatus API documentation
    */
-  public function addStatus($generation_amount = NULL, $consumption_amount = NULL, \DateTime $timestamp = NULL) {
+  public function addStatus($production_amount = NULL, $consumption_amount = NULL, \DateTime $timestamp = NULL) {
 
-    if (is_null($generation_amount) && is_null($consumption_amount)) {
-      throw new \Exception('Missing generation and consumption values at the same time. At least one must be provided.');
+    if (is_null($production_amount) && is_null($consumption_amount)) {
+      throw new \Exception('Missing both production and consumption values');
     }
 
     if (is_null($timestamp)) {
@@ -56,8 +56,8 @@ class PVOutput {
         'd' => $date,
         't' => $time,
     );
-    if (!is_null($generation_amount)) {
-      $post_fields['v2'] = $generation_amount;
+    if (!is_null($production_amount)) {
+      $post_fields['v2'] = $production_amount;
     }
     if (!is_null($consumption_amount)) {
       $post_fields['v4'] = $consumption_amount;
@@ -76,24 +76,21 @@ class PVOutput {
    * Adds a daily Output to PVOutput.org via the Add Output Service
    * 
    * @param \DateTime $date
-   * @param int $generated watt hours (g parameter in API)
-   * @param int $peak_power watts (pp parameter in API)
-   * @param \DateTime $peak_time (pt parameter in API)
-   * @param int $consumption watt hours (c parameter in API)
+   * @param int $generated
+   * @param int $peak_power
+   * @param \DateTime $peak_time
+   * @param int $consumption
    * @return boolean true if sending data is ok
    * @throws \Exception
    * @link http://www.pvoutput.org/help.html#api-addoutput API documentation
    */
-  public function addOutput(\DateTime $date = NULL, $generated = NULL, $peak_power = NULL, 
+  public function addOutput(\DateTime $date, $generated = NULL, $peak_power = NULL, 
           \DateTime $peak_time = NULL, $consumption = NULL) {
     
     if (is_null($generated) && is_null($consumption)) {
       throw new \Exception('Missing both generation and consumption values');
     }
     
-    if (is_null($date)) {
-      $date = new \DateTime();
-    }
     $post_fields = array('d' => $date->format('Ymd'));
     
     if (!is_null($generated)) {
@@ -114,19 +111,84 @@ class PVOutput {
     }
   }
   
-  private function makeRequest($url, $post_fields) {
+  
+  /**
+   * Gets a daily Output to PVOutput.org via the Get Output Service
+   * 
+   * @param \DateTime $from_date
+   * @param \DateTime $to_date
+   * @param string $aggregate
+   * @param int $limit
+   * @param int $team_id
+   * @param int $system_id
+   * @return string
+   * @throws \Exception
+   */
+  public function getOutput(\DateTime $from_date = NULL, \DateTime $to_date = NULL, $aggregate = NULL,
+          $limit = NULL, $team_id = NULL, $system_id = NULL) {
+    
+    $request_fields = array();
+    if (!is_null($from_date)) {
+      $request_fields['df'] = $from_date->format('Ymd');
+    }
+    if (!is_null($to_date)) {
+      $request_fields['dt'] = $to_date->format('Ymd');
+    }
+    if (!is_null($aggregate)) {
+      if ($aggregate != 'm' && $aggregate != 'y') {
+        throw new \Exception('Aggregate value must be \'m\' or \'y\'.');
+      }
+      $request_fields['a'] = $aggregate;
+    }
+    if (!is_null($limit)) {
+      if (!is_numeric($limit)) {
+        throw new \Exception('Limit value must be a number');
+      }
+      $request_fields['limit'] = $limit;
+    }
+    if (!is_null($team_id)) {
+      if (!is_numeric($team_id)) {
+        throw new \Exception('Team id value must be a number');
+      }
+      $request_fields['tid'] = $team_id;
+    }
+    if (!is_null($system_id)) {
+      if (!is_numeric($system_id)) {
+        throw new \Exception('System id value must be a number');
+      }
+      $request_fields['sid1'] = $system_id;
+    }
+    
+    $response = $this->makeRequest(self::PVOUTPUT_GETOUTPUT_URL, $request_fields, FALSE);
+    return $response;
+  }
+  
+  private function makeRequest($url, $request_fields = NULL, $isPost = TRUE) {
     $ch = curl_init();
-    curl_setopt_array($ch, array(
+    $curl_options = array(
         CURLOPT_URL => $url,
-        CURLOPT_POST => TRUE,
         CURLOPT_RETURNTRANSFER => TRUE,
-        CURLOPT_POSTFIELDS => http_build_query($post_fields),
         CURLOPT_HTTPHEADER => array(
             'X-Pvoutput-Apikey: ' . $this->api_key,
             'X-Pvoutput-SystemId: ' . $this->system_id,
         ),
-    ));
-    return curl_exec($ch);
+        CURLOPT_HEADER => TRUE,
+    );
+    
+    if ($isPost) {
+      $curl_options[CURLOPT_POST] = TRUE;
+      $curl_options[CURLOPT_POSTFIELDS] = http_build_query($request_fields);
+    } else {
+      $curl_options[CURLOPT_URL] = $url . '?' .http_build_query($request_fields);
+    }
+    curl_setopt_array($ch, $curl_options);
+    $response = curl_exec($ch);
+    list($header, $body) = explode("\r\n\r\n", $response, 2);
+    $header_rows = explode("\r\n", $header);
+    if ($header_rows[0] != 'HTTP/1.1 200 OK') {
+      throw new Exception('Error contacting the web service. Header returned is: ' . $header_rows[0]);
+    }
+    return $body;
   }
 
 }
